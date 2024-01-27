8a533bda24b194d2ab99d5fb68ca7735bbde7c1d
michael <me@mgrl.de>
2024-01-27 12:13:09 +0100

    improve frontpage on low resolution screens, improves https://github.com/photobooth-app/photobooth-app/discussions/162

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -11,20 +11,12 @@
     ></div>
 
     <!-- layer display processing spinner grid to show user computer working hard -->
-    <div
-      class="full-height full-width column justify-center content-center"
-      style="position: absolute"
-      v-if="showProcessing"
-    >
+    <div class="full-height full-width column justify-center content-center" style="position: absolute" v-if="showProcessing">
       <q-spinner-grid size="20em" />
     </div>
 
     <!-- layer display the countdown timer -->
-    <div
-      class="full-height full-width column justify-center content-center"
-      style="position: absolute"
-      v-if="showCountdownCounting"
-    >
+    <div class="full-height full-width column justify-center content-center" style="position: absolute" v-if="showCountdownCounting">
       <countdown-timer
         ref="countdowntimer"
         :duration="this.stateStore.duration"
@@ -33,44 +25,43 @@
     </div>
 
     <!-- layer display the front page text -->
-    <div
-      v-if="showFrontpage"
-      id="frontpage_text"
-      v-html="uiSettingsStore.uiSettings['FRONTPAGE_TEXT']"
-    ></div>
+    <div v-if="showFrontpage" id="frontpage_text" v-html="uiSettingsStore.uiSettings['FRONTPAGE_TEXT']"></div>
 
     <q-page-sticky position="bottom" :offset="[0, 25]">
       <div v-if="showFrontpage">
-        <div class="q-gutter-sm">
+        <div class="row q-gutter-sm">
           <q-btn
             v-if="uiSettingsStore.uiSettings.show_takepic_on_frontpage"
+            stack
             color="primary"
             no-caps
-            @click="takePicture()"
-            class="action-button"
+            @click="takePicture(/**/)"
+            class="action-button col-auto"
           >
-            <q-icon left name="photo_camera" />
-            <div>Take a<br />Picture!</div>
+            <q-icon name="photo_camera" />
+            <div style="white-space: nowrap" class="gt-sm">Take a Picture!</div>
           </q-btn>
           <q-btn
             v-if="uiSettingsStore.uiSettings.show_takecollage_on_frontpage"
+            stack
             color="primary"
             no-caps
             @click="takeCollage()"
-            class="action-button"
+            class="action-button col-auto"
           >
-            <q-icon left name="auto_awesome_mosaic" />
-            <div>Create<br />Collage!</div>
+            <q-icon name="auto_awesome_mosaic" />
+            <div style="white-space: nowrap" class="gt-sm">Create Collage!</div>
           </q-btn>
           <q-btn
             v-if="uiSettingsStore.uiSettings.show_takeanimation_on_frontpage"
+            stack
             color="primary"
             no-caps
             @click="takeAnimation()"
-            class="action-button"
+            class="action-button col-auto"
           >
-            <q-icon left name="gif_box" />
-            <div>Create<br />Animation!</div>
+            <q-icon name="gif_box" />
+            <div style="white-space: nowrap" class="gt-sm">Create Animation!</div>
           </q-btn>
         </div>
       </div>
@@ -79,25 +70,13 @@
     <q-page-sticky position="top-left" :offset="[25, 25]">
       <div v-if="showFrontpage">
         <div class="q-gutter-sm">
-          <q-btn
-            v-if="uiSettingsStore.uiSettings.show_gallery_on_frontpage"
-            color="primary"
-            no-caps
-            to="/gallery"
-            class="action-button"
-          >
+          <q-btn v-if="uiSettingsStore.uiSettings.show_gallery_on_frontpage" color="primary" no-caps to="/gallery" class="action-button">
             <q-icon left name="photo_library" />
-            <div>Gallery</div>
+            <div class="gt-sm">Gallery</div>
           </q-btn>
-          <q-btn
-            v-if="uiSettingsStore.uiSettings.show_admin_on_frontpage"
-            color="secondary"
-            no-caps
-            to="/admin"
-            class="action-button"
-          >
+          <q-btn v-if="uiSettingsStore.uiSettings.show_admin_on_frontpage" color="secondary" no-caps to="/admin" class="action-button">
             <q-icon left name="admin_panel_settings" />
-            <div>Admin</div>
+            <div class="gt-sm">Admin</div>
           </q-btn>
         </div>
       </div>
