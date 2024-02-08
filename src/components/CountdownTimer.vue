c60ff23a597f35f939090ea1e1111e8b45d4a0e8
michael <me@mgrl.de>
2024-02-08 19:45:48 +0100

    improve display of countdown timer.

--- src/components/CountdownTimer.vue
+++ src/components/CountdownTimer.vue
@@ -1,5 +1,6 @@
 <template>
-  <div style="width: 70vw; height: 70vh" v-show="showBox">
+  <div class="flex flex-center" style="width: 70vw; height: 70vh" v-show="showBox">
+    <div style="position: absolute; font-size: 150px" v-show="showMessage" v-html="messageText"></div>
     <q-circular-progress
       v-show="showCountdown"
       show-value
@@ -8,13 +9,12 @@
       :min="0"
       :max="this.duration"
       reverse
-      size="70vh"
+      size="100%"
       color="primary"
       class="text-secondary"
     >
       {{ parseFloat(remainingSeconds.toFixed(0)) }}
     </q-circular-progress>
-    <div v-show="showMessage" v-html="messageText"></div>
   </div>
 </template>
 
