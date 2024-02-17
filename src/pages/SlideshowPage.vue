0087e8c00cd1ea6d39a5ae135529a5559c280013
michael <me@mgrl.de>
2024-02-17 08:53:22 +0100

    css fixes

--- src/pages/SlideshowPage.vue
+++ src/pages/SlideshowPage.vue
@@ -1,14 +1,15 @@
 <template>
-  <gallery-image-detail
-    maximized
-    :showToolbar="false"
-    :slideshowTimeout="5000"
-    :slideshowUseFade="true"
-    :itemRepository="this.mediacollectionStore.collection"
-    :indexSelected="0"
-    :randomOrder="true"
-    class="full-height"
-  ></gallery-image-detail>
+  <q-page class="full-height">
+    <gallery-image-detail
+      :showToolbar="false"
+      :slideshowTimeout="5000"
+      :slideshowUseFade="true"
+      :itemRepository="this.mediacollectionStore.collection"
+      :indexSelected="0"
+      :randomOrder="true"
+      class="full-height"
+    ></gallery-image-detail>
+  </q-page>
 </template>
 
 <script>
