1646c58bae999305c457af871b49865636e73607
michael <me@mgrl.de>
2024-02-24 07:42:48 +0100

    added color renderer

---
+++ src/components/form/controls/ColorControlRenderer.vue
@@ -0,0 +1,46 @@
+<template>
+  <control-wrapper v-bind="controlWrapper" :styles="styles" :is-focused="isFocused" :applied-options="appliedOptions">
+    <q-color
+      :model-value="control.data"
+      no-header-tabs
+      no-footer
+      :rules="['anyColor']"
+      :id="control.id + '-input'"
+      :class="styles.control.input"
+      :disable="!control.enabled"
+      :autofocus="appliedOptions.focus"
+      @update:model-value="onChange"
+      @focus="isFocused = true"
+      @blur="isFocused = false"
+      :error-message="control.errors"
+      :error="control.errors != ''"
+    ></q-color>
+  </control-wrapper>
+</template>
+
+<script lang="ts">
+import { ControlElement, JsonFormsRendererRegistryEntry, rankWith, isStringControl, and, or, formatIs, optionIs } from "@jsonforms/core";
+import { defineComponent } from "vue";
+import { rendererProps, useJsonFormsControl, RendererProps } from "@jsonforms/vue";
+import { default as ControlWrapper } from "./ControlWrapper.vue";
+import { useQuasarControl } from "../util";
+
+const controlRenderer = defineComponent({
+  name: "StringControlRenderer",
+  components: { ControlWrapper },
+  props: {
+    ...rendererProps<ControlElement>(),
+  },
+  setup(props: RendererProps<ControlElement>) {
+    return useQuasarControl(useJsonFormsControl(props), (value: any) => value || undefined, 300);
+  },
+});
+
+export default controlRenderer;
+
+export const entry: JsonFormsRendererRegistryEntry = {
+  renderer: controlRenderer,
+  // https://jsonforms.discourse.group/t/how-to-register-a-new-field-type/426/2 , TODO: there is a warning about non-standard "color" format, but still works.
+  tester: rankWith(1.1, and(isStringControl, or(formatIs("color"), optionIs("format", "color")))),
+};
+</script>
--- src/components/form/controls/index.ts
+++ src/components/form/controls/index.ts
@@ -1,13 +1,21 @@
 export { default as ControlWrapper } from "./ControlWrapper.vue";
 
-export { default as StringControlRenderer } from "./StringControlRenderer.vue";
-export { default as NumberControlRenderer } from "./NumberControlRenderer.vue";
 export { default as BooleanToggleControlRenderer } from "./BooleanToggleControlRenderer.vue";
+export { default as ColorControlRenderer } from "./ColorControlRenderer.vue";
 export { default as EnumControlRenderer } from "./EnumControlRenderer.vue";
+export { default as NumberControlRenderer } from "./NumberControlRenderer.vue";
+export { default as StringControlRenderer } from "./StringControlRenderer.vue";
 
-import { entry as StringControlRendererEntry } from "./StringControlRenderer.vue";
-import { entry as NumberControlRendererEntry } from "./NumberControlRenderer.vue";
 import { entry as BooleanToggleControlRendererEntry } from "./BooleanToggleControlRenderer.vue";
+import { entry as ColorControlRendererEntry } from "./ColorControlRenderer.vue";
 import { entry as EnumControlRendererEntry } from "./EnumControlRenderer.vue";
+import { entry as NumberControlRendererEntry } from "./NumberControlRenderer.vue";
+import { entry as StringControlRendererEntry } from "./StringControlRenderer.vue";
 
-export const controlRenderers = [StringControlRendererEntry, NumberControlRendererEntry, BooleanToggleControlRendererEntry, EnumControlRendererEntry];
+export const controlRenderers = [
+  BooleanToggleControlRendererEntry,
+  ColorControlRendererEntry,
+  EnumControlRendererEntry,
+  NumberControlRendererEntry,
+  StringControlRendererEntry,
+];
