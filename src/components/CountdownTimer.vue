41a653c662a27f188bd13bec9031fd1aafcbfba7
michael <me@mgrl.de>
2024-05-21 10:26:20 +0200

    fixed scrollbar glitch during countdown.

--- src/components/CountdownTimer.vue
+++ src/components/CountdownTimer.vue
@@ -2,21 +2,25 @@
   <div v-show="showBox" id="countdown-timer-container" class="flex flex-center" style="width: 70vw; height: 70vh">
     <!-- eslint-disable-next-line vue/no-v-html -->
     <div v-show="showMessage" id="countdown-timer-message" style="position: absolute; font-size: 150px" v-html="messageText"></div>
-    <q-circular-progress
-      v-show="showCountdown"
-      :show-value="!showMessage"
-      style="width: 100%; height: 100%"
-      :value="remainingSeconds"
-      :min="0"
-      :max="startDuration"
-      reverse
-      animation-speed="100"
-      size="70vh"
-      color="primary"
-      class="text-secondary"
-    >
-      {{ parseFloat(remainingSeconds.toFixed(0)) }}
-    </q-circular-progress>
+    <div style="height: 100%">
+      <!-- div-100%height fixes the max height of the svg to render the circular process not to exhaust the painting area resulting in scrollbars-->
+      <q-circular-progress
+        v-show="showCountdown"
+        :show-value="!showMessage"
+        style="width: 100%; height: 100%"
+        :value="remainingSeconds"
+        :min="0"
+        :max="startDuration"
+        reverse
+        animation-speed="100"
+        font-size="0.5em"
+        size="70vh"
+        color="primary"
+        class="text-secondary"
+      >
+        {{ parseFloat(remainingSeconds.toFixed(0)) }}
+      </q-circular-progress>
+    </div>
   </div>
 </template>
 
