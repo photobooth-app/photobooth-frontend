daea206b5f7bcbf07dca43dc905fd2ae8dd26d80
michael <me@mgineer85.de>
2025-04-06 12:20:52 +0200

    frontend notifications translateable.

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,10 @@
   </div>
 </template>
 
+<script lang="ts">
+import { useI18n } from 'vue-i18n'
+</script>
+
 <script setup lang="ts">
 import { ref, watch, computed, onBeforeMount } from 'vue'
 import { useMainStore } from 'src/stores/main-store'
@@ -17,6 +21,7 @@ import ConnectionOverlay from './components/ConnectionOverlay.vue'
 import { useQuasar } from 'quasar'
 import type { EventSourceStatus } from '@vueuse/core'
 import { useEventSource } from '@vueuse/core'
+const { t } = useI18n()
 
 console.log('frontend build date', process.env.BUILD_DATE)
 
@@ -81,14 +86,15 @@ const initSseClient = () => {
     store.logrecords = [JSON.parse(evt.data), ...store.logrecords.slice(0, 199)]
   })
 
-  eventSource.value?.addEventListener('FrontendNotification', (evt) => {
-    // linked to SseEventFrontendNotification, event: FrontendNotification
+  eventSource.value?.addEventListener('TranslateableFrontendNotification', (evt) => {
+    // TODO: at one point in future, we can probably use generate typescript to generate types for sse also:
+    // as components['schemas']['TranslateableFrontendNotification']
     const _notification = JSON.parse(evt.data)
     console.log(_notification)
 
     $q.notify({
-      caption: _notification['caption'] || 'Notification',
-      message: _notification['message'],
+      // eslint-disable-next-line @intlify/vue-i18n/no-dynamic-keys
+      message: t('translatedFrontendNotifications.' + _notification['message_key'], _notification['context_data']),
       color: _notification['color'] || 'info',
       icon: _notification['icon'] ? `sym_o_${_notification['icon']}` : 'sym_o_info',
       spinner: _notification['spinner'] || false,
